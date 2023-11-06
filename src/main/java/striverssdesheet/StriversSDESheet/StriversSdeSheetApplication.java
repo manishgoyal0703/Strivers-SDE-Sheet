131a80c5b82f48996d5cf42cfa20ed8c7437f6d0
manish goyal <manishgoyal0703@gmail.com>
2023-11-06 21:22:35 +0530

    learning DSA Fundamentals

---
+++ src/main/java/striverssdesheet/StriversSDESheet/StriversSdeSheetApplication.java
@@ -0,0 +1,13 @@
+package striverssdesheet.StriversSDESheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StriversSdeSheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StriversSdeSheetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/striverssdesheet/StriversSDESheet/StriversSdeSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package striverssdesheet.StriversSDESheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StriversSdeSheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
